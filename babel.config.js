a79e6af0866422dc587e56d59acfc998dc0d1f52
Dylan Vann <dylan@dylanvann.com>
2021-02-10 14:51:43 -0500

    cleanup deps and babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
-    '@emotion/babel-plugin',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-transform-runtime',
   ],
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  presets: ['@babel/preset-react', '@babel/preset-flow'],
 };
